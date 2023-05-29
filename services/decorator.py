325455fad29893f5b127963e02992cd94ca85ac5
Nayan Kumar Jha <nayankumar.jha@kanini.com>
2023-05-30 02:39:27 +0530

    added logs in decorators

--- services/decorator.py
+++ services/decorator.py
@@ -1,14 +1,15 @@
 import time
 from functools import wraps
+from services.logger import logger
 
 def measure_time(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.time()
-        print(f"⏰⏰ Task '{func.__name__}' started.")
+        logger.debug(f"⏰⏰ Task '{func.__name__}' started.")
         result = func(*args, **kwargs)
         end_time = time.time()
         execution_time = end_time - start_time
-        print(f"✅⏰ Task '{func.__name__}' completed in {execution_time:.4f} seconds.")
+        logger.debug(f"✅⏰ Task '{func.__name__}' completed in {execution_time:.4f} seconds.")
         return result
     return wrapper
